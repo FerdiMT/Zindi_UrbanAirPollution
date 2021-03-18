beb4e36b08940a7df7fd23a18cd83d029e25cc85
FerdiMT <ferranmartitorrent@gmail.com>
2021-03-18 09:19:37 +0100

    Same as 3 but adding day of the week dummies

--- Changelog.md
+++ Changelog.md
@@ -2,3 +2,4 @@
 2. Taking all features that have less than 300 NAS in test set. Score: 38.4
 3. Taking all features that have less than 5000 NAS in test set. Score: 35.2
 4. With gridsearch and pipeline with standardscaler. Score: 35.55
+5. Same as 3 but adding day of the week dummies. Score: 34.88
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import pandas as pd
 from xgboost import XGBRegressor
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 
 train = pd.read_csv('Data/Train.csv')
 test = pd.read_csv('Data/Test.csv')
@@ -18,37 +14,32 @@ test.dropna(thresh=len(test) - 5000, axis=1, inplace=True)
 label_train = train['target']
 features_train = train[test.columns]
 
+# Create day of the week feature as dummy columns
+features_train['Date'] = pd.to_datetime(features_train['Date'])
+features_train['day_of_week'] = features_train['Date'].dt.day_name()
+features_train = pd.get_dummies(features_train, columns=['day_of_week'])
+test['Date'] = pd.to_datetime(test['Date'])
+test['day_of_week'] = test['Date'].dt.day_name()
+test = pd.get_dummies(test, columns=['day_of_week'])
+
+
 # We remove the additional columns that appear in train but not in test and separate label and features
 features_train = features_train.iloc[:,3:]
 features_test = test.iloc[:,3:]
 
-# Train model on all the training data.
+# Train model on all the training data. TODO: Cross validate, create train/validation split, etc...
 model = XGBRegressor()
 
-# Adding pipeline and param_grid
-pipeline = Pipeline([
-    ('standard_scaler', StandardScaler()),
-    ('model', model)
-])
-# Already searched, we leave only the parameters that are the winners after an extense gridsearch.
-param_grid = {
-    'model__max_depth': [1,2,3,4,5,6,10],
-    'model__n_estimators': [5,10,15],
-    'model__eta':[0.01,0.1,0.3],
-}
-
-grid = GridSearchCV(pipeline, param_grid, cv=3, n_jobs=-1, scoring='neg_root_mean_squared_error', verbose=2)
-
-grid.fit(features_train, label_train)
+model.fit(features_train, label_train)
 
 # Make predictions for test data
-y_pred = grid.predict(features_test)
+y_pred = model.predict(features_test)
 predictions = [round(value) for value in y_pred]
 
 # Ensamble predictions with the correct submission format
 submission = test[['Place_ID X Date']].join(pd.DataFrame(predictions))
 submission.columns = ['Place_ID X Date', 'target']
-submission.to_csv('submission2.csv', index=False)
+submission.to_csv('submission.csv', index=False)
 
 
 
